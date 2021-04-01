c12e0ca12fb656b8c552da3635a90dd6302e88a1
Liam Stewart <liamchristopher.media@gmail.com>
2021-03-31 21:12:27 -0700

    added some psuedocode.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,6 @@
 //state variables
+// I need a way to save to local storage when the save button is clicked.
+// I need to figure out a way indicate if the timeblock is in the past present or future.
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM, Do, YYYY"));
