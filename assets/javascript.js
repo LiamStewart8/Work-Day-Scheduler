b1d77c1c5874416dcdfbf661ce6306a49cec569f
Liam Stewart <liamchristopher.media@gmail.com>
2021-04-02 19:38:05 -0700

    finished the showMessage function

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,5 @@
 var saveButton = $('.saveBtn')
-var currentHour = moment().format('ha')
+var currentHour = moment()
 // var allTextBox = $('description');
 
 
@@ -15,52 +15,31 @@ saveButton.click(function (event) {
 
 //this is the function that will display the saved message to the page.
 function showMessage() {
-    //the var keys is where i take the object from keys in the local storage.
-    // var keys = Object.keys(localStorage)
-    // var i = keys.length;
-    // for (var j = 0; j < i; j++) {
-    //     document.getElementById(keys[j]).value = localStorage.getItem(keys[j])
-    // }
     var timesheet = $('#timesheet')[0]
-    for ( var i = 0; i < timesheet.children.length; i++) {
-        
-        
+    for (var i = 0; i < timesheet.children.length; i++) {
+
         var timeInputId = timesheet.children[i].id + '_input';
         var timeInput = document.getElementById(timeInputId);
         var userInput = localStorage.getItem(timeInputId)
 
         timeInput.value = userInput
-        console.log(new Date(timesheet.children[i].id) < currentHour)
-        if (moment(timesheet.children[i].id).isAfter(currentHour)) {
+
+        timesheetTime = moment(timesheet.children[i].id, 'ha')
+
+        if (moment(currentHour).isBefore(timesheetTime)) {
             timeInput.classList.add('future');
         }
-        else if(moment(currentHour).isAfter(timesheet.children[i].id)) {
+        else if (moment(currentHour).isAfter(timesheetTime)) {
             timeInput.classList.add('past');
         }
-
+        else {
+            timeInput.classList.add('present');
+        }
     }
-    
+
 }
+
 showMessage()
-console.log(currentHour)
-
-
-
-//now i need a way to compare each row to the curret time "moment()" and apply the css attributes accordingly.
-// var userInput = document.getElementById(event.target.id.replace("input"))
-
-    // for (var i = 0; i < allTextBox.length; i++) {
-    //     var divHour = parseInt($(allTextBox[i]).attr('data-value'));
-
-    // }
-
-
-// var divHour = parseInt($('description'))
-//     if (userInput < currentHour) {
-//     $('description').addClass('past');
-//     }  
-
-
 
 //this is the current date that is displayed at the top of the page.
 var today = moment();
